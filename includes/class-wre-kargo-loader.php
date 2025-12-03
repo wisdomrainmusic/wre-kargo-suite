0d198a237884642848b73ee11fe752eba9e7b452
wisdomrainmusic <alisverissepetiniz4601@gmail.com>
2025-12-03 11:29:33 +0300

    Register Basit Kargo shipping method

--- includes/class-wre-kargo-loader.php
+++ includes/class-wre-kargo-loader.php
@@ -7,6 +7,19 @@ class WRE_Kargo_Loader {
         error_log("WRE Kargo Suite: Loader initialized");
 
         add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_admin_assets' ] );
+
+        // WooCommerce Shipping Methods
+        add_filter( 'woocommerce_shipping_methods', [ $this, 'register_shipping_methods' ] );
+    }
+
+    public function register_shipping_methods( $methods ) {
+
+        require_once WRE_KARGO_SUITE_DIR . 'includes/class-wre-kargo-shipping-method-base.php';
+        require_once WRE_KARGO_SUITE_DIR . 'includes/shipping-methods/class-wc-shipping-wre-basit.php';
+
+        $methods['wre_basit_kargo'] = 'WC_Shipping_WRE_Basit';
+
+        return $methods;
     }
 
     public function enqueue_admin_assets() {
